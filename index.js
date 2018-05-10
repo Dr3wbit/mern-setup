5560c01d685c5beac1e6a7450d79cef544464e3e
Dr3wbit <eightbitandrew@gmail.com>
2018-05-10 15:26:02 -0700

    dev servers setup

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const PORT = process.env.PORT || 9000;
 
 const app = express();
 
+app.use(cors());
+
+app.get('/user-data', (req, res) => {
+    const user = {
+        name: 'Jim Bob',
+        email: 'jimthebob@gmail.com',
+    }
+
+    res.send(user);
+});
+
 app.get('/', (req, res) => {
-    res.send('<h1>App is working</h1>');
+    res.send('<h1>Ur face is working</h1>');
 });
 
 app.listen(PORT, () => {
